fb9a6e7d1123b823eea0a839c35ebc09e3983d68
Residers <fro1kov@yandex.ru>
2022-02-23 15:12:05 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <mcv-topbar />
+
+    <router-view />
+  </div>
+</template>
+<script>
+import McvTopbar from '@/components/Topbar'
+export default {
+  name: 'McvApp',
+  components: {McvTopbar},
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+import Home from '@/views/Home.vue'
+import Register from '@/views/Register'
+import Login from '@/views/Login'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+export default router
